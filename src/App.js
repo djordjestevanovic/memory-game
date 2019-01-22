2398f5e6adf35ac45fc6e87442b23749ccdb6569
djordje <djordjesteva@gmail.com>
2019-01-22 03:56:38 +0100

    added tile fliping functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  render() {
+  
+    flip(e){
+        console.log(e);
+        let front = document.querySelector('.front');
+        let back = document.querySelector('.back');
+
+        back.style.transform = "perspective(900px) rotateY(180deg)";
+        front.style.transform = "perspective(900px) rotateY(0deg)";
+
+        setTimeout(function () {
+            back.style.transform = "perspective(900px) rotateY(0deg)";
+            front.style.transform = "perspective(900px) rotateY(180deg)";
+            
+          }, 700)
+    }
+
+    render() {
+
+    const json = [
+        {
+            "id": 0,
+            "name":"react",
+            "img":"./assets/react.png"
+        },
+        {
+            "id": 1,
+            "name":"vue",
+            "img":"./assets/vue.png"
+        }
+    ]
+
+    const slike = json.map((item) => {
+        return(
+            <div className="tile" key={item.id} onClick={this.flip}>
+                <div className="front" >
+                    <img src={item.img} alt="logo" ></img>
+                </div>
+                <div className="back">
+                </div>
+            </div>
+        ) 
+    })
+
     return (
-      <div>
-        <h1>Memory game</h1>
+      <div className="gameboard">
+          {slike}
       </div>
     )
   }
